d3753aebb560750d704daf92ce51e9ec468434b7
Narayan <aayanpoudel@gmail.com>
2021-12-07 13:55:18 -0600

    .gitignore is now working

--- config/default.js
+++
@@ -1,9 +0,0 @@
-module.exports = exports = {
-  port: 3000,
-  // Contentstack Config
-  contentstack: {
-    api_key: "bltefc24c31f3b55a22",
-    delivery_token: "csfe8dd5b73695d60e2ba847e0",
-    environment: "development",
-  },
-};
